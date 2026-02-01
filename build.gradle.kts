b2fa70e2c22e06407f49e7f4cb8a6584049fef3c
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2026-02-01 22:51:12 +0300

    Version 0.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 subprojects {
     group = "ru.herobrine1st.accompanist"
-    version = "0.1.3"
+    version = "0.1.4"
 
     plugins.withId("com.vanniktech.maven.publish") {
         extensions.configure(com.vanniktech.maven.publish.MavenPublishBaseExtension::class) {
